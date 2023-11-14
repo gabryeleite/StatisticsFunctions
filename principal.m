068ca95975e6bea49c625b5bf7844aeef9bc74c0
Gabryel Leite <96426826+gabryeleite@users.noreply.github.com>
2023-11-14 20:03:38 -0300

    Update principal.m

--- principal.m
+++ principal.m
@@ -144,6 +144,94 @@ function binomial()
   grid on;
 end
 
+% Pascal:
+function pmf = pascalpmf(k, p, x)
+  % vetor pmf: pmf(i) = Prob[X = x(i)]
+
+  x = x(:);
+  n = max(x);
+  i = (k:n-1)';
+  ip = [1; (1-p) * (i./(i+1-k))];
+
+  % pb = all n-k+1
+  pb = (p^k) * cumprod(ip);
+
+  okx = (x == floor(x)) & (x >= k);
+
+  x = (okx .* x) + k * (1 - okx);
+
+  % pmf(i) = 0 a menos que x(i) >= k
+  pmf = okx .* pb(x-k+1);
+end
+
+function cdf = pascalcdf(k, p, x)
+  % cdf(i) = Prob[X <= x(i)]
+
+  x = floor(x(:));
+  allx = k:max(x);
+
+  allcdf = cumsum(pascalpmf(k, p, allx));
+
+  okx = (x >= k);
+
+  x = (okx .* x) + ((1 - okx) * k);
+  cdf = okx .* allcdf(x - k + 1);
+end
+
+function x = pascalrv(k, p, m)
+  % returna m amostras de pascal(k,p) rv
+
+  r = rand(m, 1);
+  rmax = max(r);
+  xmin = k;
+  xmax = ceil(2 * (k / p));
+
+  sx = xmin:xmax;
+  cdf = pascalcdf(k, p, sx);
+
+  while cdf(end) <= rmax
+      xmax = 2 * xmax;
+      sx = xmin:xmax;
+      cdf = pascalcdf(k, p, sx);
+  end
+
+  x = xmin + sum(cdf <= r, 2);
+end
+
+function pascal()
+  % Input:
+  k = input('Informe o parâmetro k (inteiro positivo): ');
+  p = input('Informe a probabilidade p (entre 0 e 1): ');
+  % m = input('Informe o número de amostras (m): ');
+  fid = fopen('dados.txt','r');
+  x = fscanf(fid, '%d');
+  fclose(fid);
+  %x = pascalrv(k, p, m)
+  valores_pmf = pascalpmf(k, p, x);
+  valores_cdf = pascalcdf(k, p, x);
+
+  % Output:
+  % PascalPMF
+  subplot(2, 1, 1);
+  stem(x, valores_pmf, 'LineWidth', 2);
+  title('Pascal PMF');
+  xlabel('X');
+  ylabel('Probabilidade');
+  ylim([0, 1]);
+  xticks(x);
+  grid on;
+
+  % PascalCDF
+  subplot(2, 1, 2);
+  stairs(x, valores_cdf, 'LineWidth', 2);
+  title('Pascal CDF');
+  xlabel('X');
+  ylabel('Probabilidade acumulada');
+  ylim([0, 1.5]);
+  xticks(x);
+  grid on;
+end
+
 % Poisson:
 function pmf = poisson_pmf(alpha, x)
   % Variável aleatória X seguindo a distribuição de Poisson(alpha)
@@ -230,32 +318,34 @@ end
 
 % Programa principal:
 while true
-      clc
-      % Exibir opções do menu
-      fprintf('[1] Bernoulli\n');
-      fprintf('[2] Binomial\n');
-      fprintf('[3] Uniforme\n');
-      fprintf('[4] Earlang\n');
-      fprintf('[5] Exponencial\n');
-      fprintf('[6] Finita\n');
-      fprintf('[7] Gauss\n');
-      fprintf('[8] Geometrica\n');
-      fprintf('[9] Pascal\n');
-      fprintf('[10] Poisson\n');
-      fprintf('[0] Sair\n');
-      opcao = input('Escolha: ');
+  clc
+  % Exibir opções do menu
+  fprintf('[1] Bernoulli\n');
+  fprintf('[2] Binomial\n');
+  fprintf('[3] Uniforme\n');
+  fprintf('[4] Earlang\n');
+  fprintf('[5] Exponencial\n');
+  fprintf('[6] Finita\n');
+  fprintf('[7] Gauss\n');
+  fprintf('[8] Geometrica\n');
+  fprintf('[9] Pascal\n');
+  fprintf('[10] Poisson\n');
+  fprintf('[0] Sair\n');
+  opcao = input('Escolha: ');
 
-      switch opcao
-          case 1
-              bernoulli();
-          case 2
-              binomial();
-          case 10
-              poisson();
-          case 0
-              fprintf('Programa finalizado!\n');
-              break; % Interrompe o loop
-          otherwise
-              fprintf('Opcao invalida! Tente novamente\n');
-      end
+  switch opcao
+      case 1
+          bernoulli();
+      case 2
+          binomial();
+      case 9
+          pascal();
+      case 10
+          poisson();
+      case 0
+          fprintf('Programa finalizado!\n');
+          break; % Interrompe o loop
+      otherwise
+          fprintf('Opcao invalida! Tente novamente\n');
+  end
 end
